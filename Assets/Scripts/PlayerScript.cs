9bf7a4bee15a1d0e0da4fb365bac3595ac8cb9d6
Lucas Coelho <58010418+LucasVinicius314@users.noreply.github.com>
2022-11-02 17:27:02 -0300

    Finished project

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,14 +1,100 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 #nullable enable
 
 public class PlayerScript : MonoBehaviour
 {
+  static PlayerScript instance = default!;
+
   CharacterController? controller;
+
+  [SerializeField]
+  Text? pointsText;
+  [SerializeField]
+  Text? speedText;
+
+  int points = 0;
+  bool isSpeedBoostActive = false;
+
+  List<Coroutine> coroutines = new List<Coroutine>();
+
+  public void AddPoints(int newPoints)
+  {
+    points += newPoints;
+
+    UpdatePoints();
+  }
+
+  public void BuffSpeed()
+  {
+    foreach (var item in coroutines)
+    {
+      StopCoroutine(item);
+    }
+
+    coroutines.Clear();
+
+    coroutines.Add(StartCoroutine(SpeedBuff()));
+  }
+
+  System.Collections.IEnumerator SpeedBuff()
+  {
+    isSpeedBoostActive = true;
+
+    UpdateSpeed();
+
+    yield return new WaitForSeconds(4f);
+
+    isSpeedBoostActive = false;
+
+    UpdateSpeed();
+  }
+
+  void UpdatePoints()
+  {
+    if (pointsText != null)
+    {
+      pointsText.text = points.ToString();
+    }
+  }
+
+  void UpdateSpeed()
+  {
+    if (speedText != null)
+    {
+      speedText.text = (isSpeedBoostActive ? 10f : 5f).ToString("0.00");
+    }
+  }
 
   void Awake()
   {
+    instance = this;
+
     controller = GetComponent<CharacterController>();
+  }
+
+  void OnControllerColliderHit(ControllerColliderHit hit)
+  {
+    if (hit.collider.tag == "PointsBuff")
+    {
+      AddPoints(10);
+
+      Destroy(hit.collider.gameObject);
+    }
+    else if (hit.collider.tag == "SpeedBuff")
+    {
+      BuffSpeed();
+
+      Destroy(hit.collider.gameObject);
+    }
+  }
+
+  void Start()
+  {
+    UpdatePoints();
+    UpdateSpeed();
   }
 
   void Update()
@@ -18,6 +104,6 @@ public class PlayerScript : MonoBehaviour
 
     var input = Vector3.ClampMagnitude(Vector3.forward * z + Vector3.right * x, 1f);
 
-    controller?.SimpleMove(input * 5f);
+    controller?.SimpleMove(input * (isSpeedBoostActive ? 10f : 5f));
   }
 }
